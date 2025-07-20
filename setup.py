3ed19e52f7dc0d685e363966b1ecd134be911af0
dhruva-25 <ds23adi@herts.ac.uk>
2025-07-20 09:54:47 +0100

    setup done

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name= "documnet_portal",
+    description="A document management portal built with FastAPI",
+    url= "https://github.com/Dhruv-saxena-25/documnet_portal",
+    author="Dhruv Saxena",
+    author_email= "saxena25dhruv@gmail.com",
+    version= "0.1",
+    packages= find_packages(),
+)
+
