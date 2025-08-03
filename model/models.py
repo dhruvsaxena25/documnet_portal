4956bbb6cf727cd98558199bdecc68696c8e6728
dhruva-25 <ds23adi@herts.ac.uk>
2025-08-03 10:14:12 +0100

    pydantic models

--- model/models.py
+++ model/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field, RootModel
 from typing import Optional, List, Dict, Any, Union
+from enum import Enum
 
 ## Document Analysis
 class Metadata(BaseModel):
@@ -22,3 +23,9 @@ class ChangeFormat(BaseModel):
 
 class SummaryResponse(RootModel[list[ChangeFormat]]):
     pass
+
+class PromptType(str, Enum):
+    DOCUMENT_ANALYSIS = "document_analysis"
+    DOCUMENT_COMPARISON = "document_comparison"
+    CONTEXTUALIZE_QUESTION = "contextualize_question"
+    CONTEXT_QA = "context_qa"
