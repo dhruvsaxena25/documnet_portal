60dda800b2d87596ed83258ec4a5c6e78086ef9f
dhruva-25 <ds23adi@herts.ac.uk>
2025-08-10 10:29:38 +0100

    data_ingestion

---
+++ src/document_ingestion/data_ingestion.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+
+
+
+
+class FaissManager:
+    pass
+
+
+
+class DocHandler:
+    pass
+
+
+class DocumentComparator:
+    pass
+
+
+class ChatIngestor:
+    pass
+
+
