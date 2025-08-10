3b8e60e92f77d71f3296fafd5d9dec31456b38bd
dhruva-25 <ds23adi@herts.ac.uk>
2025-08-10 10:00:28 +0100

    multi doc chat data_ingestion

--- src/multi_document_chat/data_ingestion.py
+++ src/multi_document_chat/data_ingestion.py
@@ -11,10 +11,11 @@ from utils.model_loader import ModelLoader
 
 
 class DocumentIngestor:
-    SUPPORTED_FILE_TYPES = {'.pdf', '.doc', '.docx', '.txt', '.md'}
+    SUPPORTED_EXTENSIONS = {'.pdf', '.docx', '.txt', '.md'}
     def __init__(self, temp_dir:str = "data/multi_doc_chat",faiss_dir: str = "faiss_index", session_id: str | None = None):
         try:
             self.log = CustomLogger().get_logger(__name__)
+            
             
             # base dirs
             self.temp_dir = Path(temp_dir)
@@ -30,6 +31,7 @@ class DocumentIngestor:
             self.session_faiss_dir.mkdir(parents=True, exist_ok=True)
             
             self.model_loader = ModelLoader()
+            
             self.log.info(
                 "DocumentIngestor initialized",
                 temp_base=str(self.temp_dir),
@@ -43,12 +45,13 @@ class DocumentIngestor:
             raise DocumentPortalException("Initialization error in DocumentIngestor", sys)
             
     
-    def ingest_files(self, uploaded_files):
+    def ingest_files(self,uploaded_files):
         try:
-            documents= []
+            documents=[]
+            
             for uploaded_file in uploaded_files:
                 ext = Path(uploaded_file.name).suffix.lower()
-                if ext not in self.SUPPORTED_FILE_TYPES:
+                if ext not in self.SUPPORTED_EXTENSIONS:
                     self.log.warning("Unsupported file skipped", filename=uploaded_file.name)
                     continue
                 unique_filename = f"{uuid.uuid4().hex[:8]}{ext}"
@@ -76,6 +79,7 @@ class DocumentIngestor:
                 
             self.log.info("All documents loaded", total_docs=len(documents), session_id=self.session_id)
             return self._create_retriever(documents)
+                  
         except Exception as e:
             self.log.error("Failed to ingest files", error=str(e))
             raise DocumentPortalException("Ingestion error in DocumentIngestor", sys)
@@ -83,21 +87,26 @@ class DocumentIngestor:
     def _create_retriever(self, documents):
         try:
             splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
-            chunks= splitter.split_documents(documents)
+            
+            chunks = splitter.split_documents(documents)
+            
             self.log.info("Documents split into chunks", total_chunks=len(chunks), session_id=self.session_id)
             
             embeddings = self.model_loader.load_embeddings()
+            
             vectorstore = FAISS.from_documents(documents=chunks, embedding=embeddings)
             
             # Save FAISS index under session folder
             vectorstore.save_local(str(self.session_faiss_dir))
+            
             self.log.info("FAISS index saved to disk", path=str(self.session_faiss_dir), session_id=self.session_id)
             
             retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
             
             self.log.info("FAISS retriever created and ready to use", session_id=self.session_id)
+            
             return retriever
-
+            
         except Exception as e:
             self.log.error("Failed to create retriever", error=str(e))
             raise DocumentPortalException("Retrieval error in DocumentIngestor", sys)
