fbaa479c529eb05e8e6d8df57364d2ef6abfa126
Dhruv-saxena-25 <saxena25dhruv@gmail.com>
2025-09-19 07:18:31 +0530

    config loader

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,3 +1,13 @@
+
+# import yaml
+
+
+# def load_config(config_path: str = "config/config.yaml") -> dict:
+#     with open(config_path, "r") as file:
+#         config=yaml.safe_load(file)
+#     return config
+
+# utils/config_loader.py
 from pathlib import Path
 import os
 import yaml
